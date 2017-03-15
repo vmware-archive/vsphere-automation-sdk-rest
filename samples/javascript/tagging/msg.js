48feb6486398b900639c3db722a79f343b556fd8
J.R. Garcia <jr@garciaole.com>
2017-03-15 17:33:09 -0500

    Fix typo in license header
    Change typo of SODX to SPDX for the short license identifer

--- samples/javascript/tagging/msg.js
+++ samples/javascript/tagging/msg.js
@@ -1,7 +1,7 @@
 /*
  * *******************************************************
  * Copyright VMware, Inc. 2014.  All Rights Reserved.
- * SODX-License-Identifier: MIT
+ * SPDX-License-Identifier: MIT
  * *******************************************************
  *
  * DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
