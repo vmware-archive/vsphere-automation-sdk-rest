4f347e39e8ce474d92222a1a04dfd3917060b69c
yadavk <yadavk2@vmware.com>
2019-04-23 04:36:31 +0000

    Updated REST doc for VMware Cloud on AWS console APIs

--- vmc/restdocutils.js
+++ vmc/restdocutils.js
@@ -29,11 +29,14 @@ $(function() {
     });
 });
 
-function showFields() {
-    document.getElementById( "fields1" ).className = '';
-    document.getElementById( "fields2" ).className = 'hidden';
-}
-function showLegacyFields() {
-    document.getElementById( "fields2" ).className = '';
-    document.getElementById( "fields1" ).className = 'hidden';
+function setMaxHeight(ids) {
+    var divHeight = -1;
+    for (i = 0; i < ids.length; ++i) {
+        if ($(ids[i]).height() > divHeight) {
+            divHeight = $(ids[i]).height();
+        }
+    }
+    for (i = 0; i < ids.length; ++i) {
+        $(ids[i]).height(divHeight);
+    }
 }
